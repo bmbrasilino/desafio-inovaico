53843ae1a7d7143c11557e3679507f7683277202
bmbrasilino <bianca_maciel.c@hotmail.com>
2021-09-07 18:47:34 -0300

    feat: adiciona componente navBar

---
+++ src/pages/components/navBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Nav () {
+    return (
+        <nav>
+            <div className="nav-wrapper">
+                <a href="/dashboard" className="brand-logo">PatientCareApp</a>
+            </div>
+        </nav>
+    );
+}
